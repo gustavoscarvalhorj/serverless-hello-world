ddeeedf483ab5e4ec1ef4f839e84bd3392f84be3
Gustavo Carvalho <gustavocarvalhogscrj@gmail.com>
2019-11-10 16:50:42 -0300

    Botando a aplicação para rodar localmente

--- handler.js
+++ handler.js
@@ -1,13 +1,4 @@
 'use strict';
+const app = require('./index');
 const serverless = require("serverless-http");
-const hbs = require("hbs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.set("view engine", "hbs");app.get("/", function(req, res) {
-  res.status(200).render("index");
-});
 module.exports.helloWorld= serverless(app);
---
+++ local.js
@@ -0,0 +1,4 @@
+const app = require('./index');
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!');
+});
